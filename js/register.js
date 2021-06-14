92419002ea6377a86b88aa200f32c79422d0f6c2
Natã Vitoriano de Almeida Silva <natavitoriano@hotmail.com>
2021-06-14 16:05:47 -0300

    Updating hidden menu

--- js/register.js
+++ js/register.js
@@ -8,11 +8,13 @@ btnHeader.onclick = (e) =>{
     if(iconMenu.style.display != 'none'){
         iconMenu.style.display = 'none'
         iconClose.style.display = 'block'
-        hiddenMenu.style.display = 'block'
+        hiddenMenu.classList.remove('menu-display-none')
+        hiddenMenu.classList.add('menu-display-block')
     }
     else{
         iconMenu.style.display = 'block'
         iconClose.style.display = 'none'
-        hiddenMenu.style.display = 'none'
+        hiddenMenu.classList.remove('menu-display-block')
+        hiddenMenu.classList.add('menu-display-none')
     }
 }
