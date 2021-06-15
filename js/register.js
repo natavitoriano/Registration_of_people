903226e2ef6990a24bb530e156f5cb357520a31f
Natã Vitoriano de Almeida Silva <natavitoriano@hotmail.com>
2021-06-15 19:59:12 -0300

    Adding click function to menus

--- js/register.js
+++ js/register.js
@@ -1,3 +1,26 @@
+window.onload = () => {
+    const menuBarLink = document.getElementsByClassName('menu-bar-link')
+    const menuSmallLink = document.getElementsByClassName('menu-small-device-link')
+
+    btnMenuClick(menuBarLink, 'menu-bar-active')
+    btnMenuClick(menuSmallLink, 'menu-small-active')
+}
+
+//function that places click events on menus 
+const btnMenuClick = (btnClass, btnClassActive) => {
+    if(window.location.href.indexOf('index.html') != -1)
+        btnClass[0].classList.add(btnClassActive)
+    Array.from(btnClass).forEach(btn => {
+        btn.onclick = e => {
+            e.preventDefault()
+            const previousMenuLink = document.querySelector('.' + btnClassActive)
+            previousMenuLink.classList.remove(btnClassActive)
+            btn.classList.add(btnClassActive)
+        }
+    })
+}
+
+//Event header button
 const btnHeader = document.querySelector('#btn-header')
 btnHeader.onclick = (e) =>{  
     e.preventDefault()
@@ -5,6 +28,7 @@ btnHeader.onclick = (e) =>{
     const iconClose = document.querySelector('#icon-close')
     const hiddenMenu = document.querySelector('#hidden-menu')
     
+    //Check if the menu icon is showing
     if(iconMenu.style.display != 'none'){
         iconMenu.style.display = 'none'
         iconClose.style.display = 'block'
@@ -18,3 +42,7 @@ btnHeader.onclick = (e) =>{
         hiddenMenu.classList.add('menu-display-none')
     }
 }
+
+
+
+
