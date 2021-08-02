f5e174d79ae8234fe9d51a339c66a7309bd9a50f
Natã Vitoriano de Almeida Silva <natavitoriano@hotmail.com>
2021-08-02 11:53:27 -0300

    Chage in menu logic, insert commit events, return user message, focus events in fields and check if they are empty

--- js/register.js
+++ js/register.js
@@ -3,66 +3,33 @@ window.onload = () => {
     const menuBarLink = document.getElementsByClassName('menu-bar-link')
     const menuSmallLink = document.getElementsByClassName('menu-small-device-link')
 
-    const homePage = document.querySelector('.menu-bar-link')
-    const content = document.querySelector('#content')
-    fetch(homePage.getAttribute('href'))
-        .then(resp => resp.text())
-        .then(html => content.innerHTML = html)
-
+    formOnSubmit();
+    focusInput('#register-name', '#register-message-name')
+    focusInput('#register-email', '#register-message-email')
+    focusInput('#register-country', '#register-message-country')
+    focusInput('#register-city', '#register-message-city')
     btnMenuClick(menuBarLink, 'menu-bar-active')
     btnMenuClick(menuSmallLink, 'menu-small-active')   
 }
 
 //function that places click events on menus 
 const btnMenuClick = (btnClass, btnClassActive) => {
-    const content = document.querySelector('#content')
-    const menuBarLink = document.getElementsByClassName('menu-bar-link')
-    const menuSmallLink = document.getElementsByClassName('menu-small-device-link')
     if(window.location.href.indexOf('index.html') != -1)
         btnClass[0].classList.add(btnClassActive)
+    if(window.location.href.indexOf('register.html') != -1)
+        btnClass[1].classList.add(btnClassActive)
+    if(window.location.href.indexOf('control.html') != -1)
+        btnClass[2].classList.add(btnClassActive)    
     Array.from(btnClass).forEach(btn => {
         btn.onclick = e => {
             e.preventDefault()
-
+            
             //check if the clicked link is the same as the previous one
             const previousMenuLink = document.querySelector('.' + btnClassActive)
             if(previousMenuLink.text != btn.text){
-                //deselect the previous link and select the current one
-                previousMenuLink.classList.remove(btnClassActive)
-                btn.classList.add(btnClassActive)
-
-                //hide the hidden menu if any link is clicked
-                const iconMenu = document.querySelector('#icon-menu')
-                const iconClose = document.querySelector('#icon-close')
-                const hiddenMenu = document.querySelector('#hidden-menu')
-                iconMenu.style.display = 'block'
-                iconClose.style.display = 'none'
-                hiddenMenu.classList.remove('menu-display-block')
-                hiddenMenu.classList.add('menu-display-none')
-
-                //redirects the content of the clicked link to the main container of the main page
-                fetch(btn.getAttribute('href'))
-                    .then(resp => resp.text())
-                    .then(html => content.innerHTML = html)
-
-                //identifies which menu was used, and selects the same option in the other menu
-                if(btnClassActive === 'menu-bar-active')
-                    otherMenu(menuSmallLink, 'menu-small-active', btn.text)
-                else
-                    otherMenu(menuBarLink, 'menu-bar-active', btn.text)    
-        }
+                location.href = btn.href
+            }
     }
-    })
-}
-
-//function to leave both menus with the same selection
-const otherMenu = (btnClass, btnClassActive, text) => {
-    Array.from(btnClass).forEach(btn => {
-        if(btn.text == text){
-            const previousMenuLink = document.querySelector('.' + btnClassActive)
-            previousMenuLink.classList.remove(btnClassActive)
-            btn.classList.add(btnClassActive)
-        }
     })
 }
 
@@ -86,6 +53,121 @@ btnHeader.onclick = (e) =>{
         iconClose.style.display = 'none'
         hiddenMenu.classList.remove('menu-display-block')
         hiddenMenu.classList.add('menu-display-none')
+    }
+}
+
+//form data submission event
+function formOnSubmit (){
+    const formRegister = document.querySelector('#form-register')
+    const msg = document.querySelector('#register-message')
+    try{
+                formRegister.onsubmit = function(e){
+                    e.preventDefault();
+                    inputBorder('#register-name', '#register-message-name')
+                    inputBorder('#register-email', '#register-message-email')
+                    inputBorder('#register-country', '#register-message-country')
+                    inputBorder('#register-city', '#register-message-city')
+                    if(emptyOrNull("#register-name") || emptyOrNull("#register-email") || emptyOrNull("#register-country") || emptyOrNull("#register-city")) {
+                        messageForm('failed', 'Error: fill in all fields!')
+                    }else{
+                    const url = 'http://localhost:3000/auth/register';
+                    formRegister.action = url;
+                    const formdata = new FormData(formRegister);
+                    const data = `{"name":"${formdata.get('name')}","email":"${formdata.get('email')}","country":"${formdata.get('country')}","city":"${formdata.get('city')}"}`;
+                    const jsondata = JSON.parse(data)
+
+                    fetch(url,{
+                        method: 'POST',
+                        body: JSON.stringify(jsondata),
+                        headers: {
+                            "Content-type": "application/json; charset=UTF-8"
+                        }
+                    }).then(resp => resp.json().then(data => ({status: resp.status, body: data})))
+                    .then(user => {
+                        if(user.status === 400){
+                            messageForm('failed', user.body.error)
+                            if(user.body.error === 'User alredy exists'){
+                                cleanFields('#register-email')  
+                                inputBorder('#register-email', '#register-message-email')
+                            }     
+                        }
+                        else{
+                            messageForm('success',`Thank you for registering ${user.body.name}!`)
+                        }
+                    }).catch(err => console.log(err))
+                }
+            }
+            }
+            catch(err){
+                
+            }
+}
+
+//function that shows a message to the user if an error occurred or if the data was sent correctly
+const messageForm = (sucOrFail, strMsg) => {
+    const msg = document.querySelector('#register-message')
+    if(sucOrFail === 'failed'){
+        msg.textContent = strMsg
+        msg.classList.remove('register-message-success')
+        msg.classList.add('register-message-failed')
+        alert(strMsg)
+    }else{
+        msg.textContent = strMsg
+        msg.classList.add('register-message-success')
+        msg.classList.remove('register-message-failed')
+        alert(strMsg)
+
+        cleanFields('#register-name')
+        cleanFields('#register-email')
+        cleanFields('#register-country')
+        cleanFields('#register-city')
+    }
+    msg.style.display = 'block'
+}
+
+//function to clear the fields
+const cleanFields = (field) => {
+    document.querySelector(field).value = ""
+    document.querySelector(field).classList.remove('input-register-no-empty')
+}
+
+//function that checks if fields are empty or null
+const emptyOrNull = (input) => {
+    const inpt = document.querySelector(input).value
+    return inpt == null || inpt.trim() == ""
+}
+
+//function to place out of focus event in fields
+const focusInput = (input, idMsg) => {
+    try{
+        const inpt = document.querySelector(input)
+        inpt.addEventListener('focusout', (e) => {
+           inputBorder(input, idMsg)
+        })
+    }catch(err){
+       
+    }
+}
+
+//function that checks and changes the color of the field if it is empty or null
+const inputBorder = (input, idMsg) => {
+    try{
+        const inpt = document.querySelector(input)
+        const msg = document.querySelector(idMsg)
+
+        if(!emptyOrNull(input)){
+            inpt.classList.remove('input-register-empty')
+            inpt.classList.remove('input-register-focus')
+            inpt.classList.add('input-register-no-empty')
+            msg.style.display = 'none'
+        } else {
+            inpt.classList.add('input-register-empty')
+            inpt.classList.add('input-register-focus')
+            inpt.classList.remove('input-register-no-empty')
+            msg.style.display = 'block'
+        }
+    } catch(err) {
+
     }
 }
 
